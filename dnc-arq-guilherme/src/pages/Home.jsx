0ed17695af1730e3ada269197d4ceda082534ea3
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-12-21 16:17:24 -0300

    adicionando banner

--- dnc-arq-guilherme/src/pages/Home.jsx
+++ dnc-arq-guilherme/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import Header from "../components/header/Header";
 import Footer from '../components/footer/Footer';
+import Button from "../components/Button/Button";
+import Banner from "../components/Banner/Banner"; 
 
 function Home() { 
     return (
         <>
-            <Header />
+            <Header /> 
             <h1>Home</h1>
             <p>Subtitle</p>
+            <Button buttonStyle="primary" arrow>Olá!</Button>
+            <Banner title="Home" image="About.jpg"/>
             <Footer />
         </>
     )
